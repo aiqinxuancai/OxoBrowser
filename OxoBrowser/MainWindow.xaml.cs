ad1f854dd89e527a3d5f1c65cdab17897bfb05c3
爱琴炫彩 <aiqinxuancai@163.com>
2017-10-11 18:44:31 +0800

    chrome版本测试

--- OxoBrowser/MainWindow.xaml.cs
+++ OxoBrowser/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using Base;
 using System.Diagnostics;
 using System.Windows.Controls.Primitives;
 using System.Reflection;
+using System.IO;
 
 namespace OxoBrowser
 {
@@ -44,6 +45,37 @@ namespace OxoBrowser
             //webMain.Navigate("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=486104/"); //花骑士
             webMain.Navigate("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=825012/"); //刀剑
             //webMain.Navigate("https://www.whatismybrowser.com/"); //花骑士
+
+
+            webMain.Visibility = Visibility.Hidden;
+
+            var setting = new CefSharp.CefSettings()
+            {
+                CachePath = Directory.GetCurrentDirectory() + @"\Cache",
+            };
+
+            setting.Locale = "zh-CN";
+            setting.UserAgent = "Mozilla/6.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2228.0 Safari/537.36";
+            setting.CefCommandLineArgs.Add("enable-npapi", "1");
+            setting.CefCommandLineArgs.Add("--proxy-server", "http://127.0.0.1:8222");
+            setting.CefCommandLineArgs.Add("--enable-media-stream", "1");
+            //setting.CefCommandLineArgs.Add("disable-gpu", "0");
+            //setting.
+            setting.CefCommandLineArgs.Add("disable-gpu", "1");
+            setting.CefCommandLineArgs.Add("disable-gpu-compositing", "1");
+            setting.CefCommandLineArgs.Add("enable-begin-frame-scheduling", "1");
+            setting.CefCommandLineArgs.Add("enable-media-stream", "1");
+
+            setting.CefCommandLineArgs["enable-system-flash"] = "0";
+            //setting.CefCommandLineArgs.Add("enable-system-flash", "0"); //Automatically discovered and load a system-wide installation of Pepper Flash.
+            setting.CefCommandLineArgs.Add("ppapi-flash-path", @".\plugins\pepflashplayer64_23_0_0_162.dll"); //Load a specific pepper flash version (Step 1 of 2)
+            setting.CefCommandLineArgs.Add("ppapi-flash-version", "23.0.0.162"); //Load a specific pepper flash version (Step 2 of 2)
+            CefSharp.Cef.Initialize(setting);
+
+            var chromeMain = new CefSharp.Wpf.ChromiumWebBrowser();
+            this.Content = chromeMain;
+            chromeMain.Address = "http://html5test.com/";
+            //chromeMain.Address = "http://www.dmm.com/netgame/social/-/gadgets/=/app_id=798209/";
 
         }
 
