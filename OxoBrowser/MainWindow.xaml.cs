e2b4cc67bde8a929aab22df1c9dd2eca009b9338
爱琴炫彩 <aiqinxuancai@163.com>
2018-09-25 12:37:23 +0800

    删除之前的IE相关函数

--- OxoBrowser/MainWindow.xaml.cs
+++ OxoBrowser/MainWindow.xaml.cs
@@ -48,14 +48,11 @@ namespace OxoBrowser
         {
             InitUI();
             UpdataSoundButton();
-            WebViewConfig.SetWebBrowserSilent(webMain, true);
-            WebBrowserZoomInvoker.AddZoomInvoker(webMain);
+
             //webMain.Navigate("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=486104/"); //花骑士
             //webMain.Navigate("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=825012/"); //刀剑
             //webMain.Navigate("https://www.whatismybrowser.com/"); //花骑士
 
-            webMain.Visibility = Visibility.Hidden;
-            
             var setting = new CefSharp.CefSettings()
             {
                 CachePath = Directory.GetCurrentDirectory() + @"\Cache",
@@ -166,112 +163,6 @@ namespace OxoBrowser
             }
         }
 
-        /// <summary>
-        /// 游戏窗口缩放、只支持IE模式，Chrome还没支持
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void menuGameSize_Click(object sender, RoutedEventArgs e)
-        {
-            MenuItem item = (MenuItem)sender;
-            try
-            {
-                string sizeString = item.Header.ToString();
-                AppConfig.m_config.WebSize = sizeString;
-                AppConfig.Save();
-                this.btnTitelWindowSize.Content = sizeString;
-                int SizeNew = int.Parse(sizeString.Replace("%", ""));
-
-                UpdataWindowSize(SizeNew);
-                System.Windows.Forms.Screen screenSize = System.Windows.Forms.Screen.FromHandle(webMain.Handle);
-                if (winMain.MinWidth > screenSize.Bounds.Width | webMain.Height + 100 > screenSize.Bounds.Height)
-                {
-                    MessageBox.Show("你设置的尺寸超出了屏幕的大小！还原回100%！", "ERROR");
-                    if (SizeNew != 100)
-                    {
-                        UpdataWindowSize(100);
-                    }
-                    winMain.Height = webMain.Height ; //+30
-                    winMain.Width = winMain.MinWidth;
-                }
-                else
-                {
-                    winMain.Height = webMain.Height ; //+30
-                    winMain.Width = winMain.MinWidth;
-                }
-            }
-            catch (System.Exception ex)
-            {
-                Debug.WriteLine(ex);
-            }
-        }
-
-
-        //static readonly int OLECMDEXECOPT_DODEFAULT = 0;
-        //static readonly int OLECMDID_OPTICAL_ZOOM = 63;
-
-        //static void SetZoom(WebBrowser webbrowser, int zoom)
-        //{
-        //    try
-        //    {
-        //        System.Drawing.PointF scaleUI = WebBrowserZoomInvoker.GetCurrentDIPScale();
-        //        if (100 != (int)(scaleUI.X * 100))
-        //        {
-        //            zoom = (int)(scaleUI.X * scaleUI.Y * zoom);
-        //        }
-        //        if (null == webbrowser)
-        //        {
-        //            return;
-        //        }
-
-        //        FieldInfo fiComWebBrowser = webbrowser.GetType().GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
-        //        if (null != fiComWebBrowser)
-        //        {
-        //            Object objComWebBrowser = fiComWebBrowser.GetValue(webbrowser);
-
-        //            if (null != objComWebBrowser)
-        //            {
-        //                object[] args = new object[]
-        //                    {
-        //                    OLECMDID_OPTICAL_ZOOM,
-        //                    OLECMDEXECOPT_DODEFAULT,
-        //                    zoom,
-        //                    IntPtr.Zero
-        //                    };
-        //                objComWebBrowser.GetType().InvokeMember(
-        //                "ExecWB",
-        //                BindingFlags.InvokeMethod,
-        //                null, objComWebBrowser,
-        //                args);
-        //            }
-        //        }
-        //    }
-        //    catch (System.Exception ex)
-        //    {
-        //    }
-        //}
-
-        private void UpdataWindowSize(double _site)
-        {
-            double __site = _site / 100; // + dpi_site
-            Debug.WriteLine(__site);
-
-            //SetZoom(webMain, (int)_site);//+ dpi_site * 100
-
-            
-            double h = Convert.ToDouble(AppConfig.m_config.FlashHeight);
-            double w = Convert.ToDouble(AppConfig.m_config.FlashWidth);
-            webMain.Height = h * (__site);
-            webMain.Width = w * (__site);
-
-            winMain.MinHeight = webMain.Height;
-            winMain.MinWidth = webMain.Width;
-
-            if (_site != 100)
-            {
-                //SetTimerWebSite();
-            }
-        }
 
         public void ToggleFlyout(int index)
         {
@@ -295,6 +186,7 @@ namespace OxoBrowser
             AppConfig.Save();
             UpdataSoundButton();
         }
+
         private void btnTitelSoundClose_Click(object sender, RoutedEventArgs e)
         {
             AppConfig.m_config.SoundClose = false;
@@ -302,23 +194,9 @@ namespace OxoBrowser
             UpdataSoundButton();
         }
 
-        private void webMain_Navigated(object sender, NavigationEventArgs e)
-        {
-            WebViewConfig.ApplyStyleSheet((mshtml.HTMLDocument) webMain.Document);
-        }
-
         private void btnTitelFlashMin_Click(object sender, RoutedEventArgs e)
         {
-
             //GetKanColle2ndHtml5Core();
-        }
-
-
-
-
-        private void webMain_Navigating(object sender, NavigatingCancelEventArgs e)
-        {
-            Debug.WriteLine(e.Uri.ToString());
         }
 
         private void btnTitelWindowTop_Checked(object sender, RoutedEventArgs e)
@@ -355,6 +233,11 @@ namespace OxoBrowser
         private void btnTitelReload_Click(object sender, RoutedEventArgs e)
         {
             ChromeWindow.thisWindow.chromeMain.GetBrowser().Reload();
+        }
+
+        private void menuGameSize_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
