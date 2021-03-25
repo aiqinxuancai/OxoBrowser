8c7df4f6fee9f3f51f92f3078297ac32306502fc
aiqinxuancai <aiqinxuancai@163.com>
2021-03-25 16:14:12 +0800

    修复catch

--- OxoBrowser/Base/WebViewConfig.cs
+++ OxoBrowser/Base/WebViewConfig.cs
@@ -19,45 +19,52 @@ namespace Base
 
         public static void GetKanColle2ndHtml5Core(CefSharp.Wpf.ChromiumWebBrowser browser)
         {
-            browser.ExecuteScriptAsync("var node = document.createElement('style'); " +
-                "node.innerHTML = 'html, body, iframe {overflow:hidden;margin:0;}'; " +
-                "document.body.appendChild(node);");
-
-            browser.ExecuteScriptAsync("var node = document.createElement('style'); " +
-                "node.innerHTML = 'game_frame {position:fixed; left:50%; top:0px; margin-left:-480px; z-index:1;}'; " +
-                "document.body.appendChild(node);");
-
-            browser.ExecuteScriptAsync("var node = document.createElement('style'); " +
-                "node.innerHTML = 'ul.area-menu {display: none;}'; " +
-                "document.body.appendChild(node);");
-            browser.ExecuteScriptAsync("var node = document.createElement('style'); " +
-                "node.innerHTML = '.dmm-ntgnavi {display: none;}'; " +
-                "document.body.appendChild(node);");
-
-            var game_frame = browser.GetBrowser().GetFrame("game_frame");
-            var source = game_frame?.GetSourceAsync();
-            source?.Wait();
-
-
-            //Browser.SetZoomLevel(Math.Log(zoomFactor, 1.2));
-
-
-            //if (StyleSheetApplied)
-            //{
-            //    Browser.Size = Browser.MinimumSize = new Size(
-            //        (int)(KanColleSize.Width * zoomFactor),
-            //        (int)(KanColleSize.Height * zoomFactor)
-            //        );
-
-            //    CenteringBrowser();
-            //}
-            //"overlap-contents"
-            //var contents_iframe = browser.GetBrowser().GetFrame("contents_iframe");
-            //var list2 = game_frame.Browser.GetFrameNames();
-            //var list = browser.GetBrowser().GetFrameNames();
-            //browser.GetBrowser();
-
-            game_frame?.ExecuteJavaScriptAsync("document.getElementById('spacing_top').style.height = '0px'");
+            try
+            {
+	            browser.ExecuteScriptAsync("var node = document.createElement('style'); " +
+	                "node.innerHTML = 'html, body, iframe {overflow:hidden;margin:0;}'; " +
+	                "document.body.appendChild(node);");
+	
+	            browser.ExecuteScriptAsync("var node = document.createElement('style'); " +
+	                "node.innerHTML = 'game_frame {position:fixed; left:50%; top:0px; margin-left:-480px; z-index:1;}'; " +
+	                "document.body.appendChild(node);");
+	
+	            browser.ExecuteScriptAsync("var node = document.createElement('style'); " +
+	                "node.innerHTML = 'ul.area-menu {display: none;}'; " +
+	                "document.body.appendChild(node);");
+	            browser.ExecuteScriptAsync("var node = document.createElement('style'); " +
+	                "node.innerHTML = '.dmm-ntgnavi {display: none;}'; " +
+	                "document.body.appendChild(node);");
+	
+	            var game_frame = browser.GetBrowser().GetFrame("game_frame");
+	            var source = game_frame?.GetSourceAsync();
+	            source?.Wait();
+	
+	
+	            //Browser.SetZoomLevel(Math.Log(zoomFactor, 1.2));
+	
+	
+	            //if (StyleSheetApplied)
+	            //{
+	            //    Browser.Size = Browser.MinimumSize = new Size(
+	            //        (int)(KanColleSize.Width * zoomFactor),
+	            //        (int)(KanColleSize.Height * zoomFactor)
+	            //        );
+	
+	            //    CenteringBrowser();
+	            //}
+	            //"overlap-contents"
+	            //var contents_iframe = browser.GetBrowser().GetFrame("contents_iframe");
+	            //var list2 = game_frame.Browser.GetFrameNames();
+	            //var list = browser.GetBrowser().GetFrameNames();
+	            //browser.GetBrowser();
+	
+	            game_frame?.ExecuteJavaScriptAsync("document.getElementById('spacing_top').style.height = '0px'");
+            }
+            catch (System.Exception ex)
+            {
+            	
+            }
 
         }
     }
